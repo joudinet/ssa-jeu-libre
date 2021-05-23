a34f4f297436b28a39be51cb9593e8842c4c0346
Johan Oudinet <johan.oudinet@gmail.com>
2021-05-24 00:53:25 +0200

    config.php: fix variable syntax

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 // MySQL connection data
 // Host
-mysql_host='localhost';
+$mysql_host='localhost';
 // Database name
-mysql_dbname=;
+$mysql_dbname='';
 // Credentials
-mysql_user=;
-mysql_pass=;
+$mysql_user='';
+$mysql_pass='';
 
 // Mail data
-mail_from='info@example.com';
-mail_fromName='Example';
+$mail_from='info@example.com';
+$mail_fromName='Example';
 ?>
