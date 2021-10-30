3f2e333cc0103df862b5660f5d412948c165d97b
Jerome Nizon <jerome.nizon@gmail.com>
2021-10-30 11:37:02 +0200

    gestion bug base de données

--- admin/gestion_fonctions.php
+++ admin/gestion_fonctions.php
@@ -108,7 +108,6 @@ function lecture_staff() {
     try {
         $tab_res=[];
         $stmt=$dbh->query('SELECT * FROM STAFF ORDER BY nom');
-        $stmt->execute();
         while ($row=$stmt->fetch()) {
             $tab_res[$row['id']]=['nom'=>$row['nom'],'mail'=>$row['mail'],'telephone'=>$row['telephone']];
         }
@@ -241,8 +240,8 @@ function remiseazero() {
         $dbh->query('DROP TABLE IF EXISTS GESTIONCRENEAUX');
         $dbh->query($msg);
         $msg='CREATE TABLE STAFF ( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,nom VARCHAR (50), ';
-        $msg.='mail VARCHAR (200), telephone VARCHAR (50), password VARCHAR (100)),';
-        $msg.='nonce VARCHAR (200), noncetime INT';
+        $msg.='mail VARCHAR (200), telephone VARCHAR (50), password VARCHAR (100),';
+        $msg.='nonce VARCHAR (200), noncetime INT)';
         $dbh->query('DROP TABLE IF EXISTS STAFF');
         $dbh->query($msg);
         $h = fopen('../staff/.htpasswd', "w");
