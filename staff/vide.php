cc14891fba1300538d55edf2e1036883ce759061
Johan Oudinet <johan.oudinet@gmail.com>
2021-11-10 16:21:26 +0100

    staff: do not send emails to ca@sandsystem.com

--- staff/vide.php
+++ staff/vide.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require "../general_fonctions.php";
 use PHPMailer\PHPMailer\PHPMailer; //pour le mail
@@ -8,26 +8,25 @@ require '../PHPMailer/src/SMTP.php';
 require '../PHPMailer/src/Exception.php';
 
 
-try { 
+try {
         $mail = new PHPmailer();
         $mail->CharSet = 'UTF-8';
         $mail->setFrom($mail_from, $mail_fromName);
         $mail->ContentType = 'text/plain';
         $mail->Subject='Désistement de staff';
-        $msg="Attention :\n\nLe créneau suivant n'a plus de staff en gestion : \n " ;
-	$msg.=$_POST['titre'];
+        $msg="Attention :\n\nLe créneau suivant n'a plus de staff en gestion :\n";
+        $msg.=$_POST['titre'];
         $mail->Body=$msg;
         $mail->AddAddress($mail_from);
-        $mail->AddAddress('ca@sandsystem.com');
         if (!$mail->send()) {
             echo json_encode("pas bon"); //'Mailer error: ' . $mail->ErrorInfo);
         } else {
-	    echo json_encode("fin");
-	}
+        echo json_encode("fin");
+    }
         $mail->SmtpClose();
-        unset($mail); 
+        unset($mail);
     } catch (Exception $e) {
         echo json_encode("Erreur dans l'envoi de mail pour prévenir l'admin");
     }
-    
+
 ?>
